a78cad035915793f304e50b0808907dd018a7d53
Kevin Systrom <kevin@burbn.com>
2011-02-24 01:42:52 -0800

    Fix for non-webkit browsers

--- public/static/scripts/master.js
+++ public/static/scripts/master.js
@@ -9,19 +9,23 @@ var Media = {
     onNewMedia: function(ev) {
         $(ev.media).each(function(index, media){
             $('<img/>').attr('src', media.images.low_resolution.url).load(function(){
-        		$newCube = $('<div class="cube in"><span class="location"></span><span class="channel"></span</div>');
-        		$newCube.prepend(this);
-        		$('.location', $newCube).html(media.location.name);
-        		$('.channel', $newCube).html(media.meta.location);
-        		var numChildren = $('#wrapper').children().length;
+                var numChildren = $('#wrapper').children().length;
         		var index = Math.floor(Math.random() * numChildren);
         		var $container = $($('#wrapper').children()[index]);
         		var $oldCube = $('.cube', $container);
-        		$container.addClass('animating').append($newCube);
-        		$oldCube.addClass('out').bind('webkitAnimationEnd', function(){
-        			$container.removeClass('animating');
-        			$(this).remove();
-        		});
+                if ($.browser.webkit){
+            		$newCube = $('<div class="cube in"><span class="location"></span><span class="channel"></span</div>');
+            		$newCube.prepend(this);
+            		$('.location', $newCube).html(media.location.name);
+            		$('.channel', $newCube).html(media.meta.location);
+            		$container.addClass('animating').append($newCube);
+            		$oldCube.addClass('out').bind('webkitAnimationEnd', function(){
+            			$container.removeClass('animating');
+            			$(this).remove();
+            		});
+    		    } else {
+    		        $oldCube.attr('src', media.images.low_resolution.url);
+    		    }
         	}); 
         });
     },
